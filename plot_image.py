96e7360ba172e3f50e525ae9ed975ae5e9a34b63
gr3gbyrne <gr3g.byrne@gmail.com>
2016-03-16 00:19:50 -0400

    Adding code to plot a 2-d color image with colorbar and write it to a png file

---
+++ plot_image.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+import sys
+import numpy as np
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+import math
+
+# parse input file
+def parse(filename,column):
+    
+    data = []
+    for line in open(filename).readlines():
+        d = line.split()
+        data.append( float(d[column]) )
+    
+    return data
+
+#Argument parsing
+if len(sys.argv)==1:
+    name = sys.argv[0].split("/")[-1]
+    print "Usage:"
+    print "{0} <column> <filename> ".format(name)
+    print "\nExample:"
+    print "{0} 2 myfile.dat".format(name)
+    exit(0)
+
+filename = sys.argv[2]
+columnread= sys.argv[1]
+imagedata=parse(filename,2)
+imagedata=np.array(imagedata)
+
+N=math.sqrt(len(imagedata))
+imagedata=np.reshape(imagedata,(N,N))
+imagedata=np.log10(imagedata)
+
+fig = plt.figure(2)
+cmap2 = mpl.colors.LinearSegmentedColormap.from_list('my_colormap',
+                                           ['blue','black','red'],
+                                           256)
+img2 = plt.imshow(imagedata,interpolation='nearest',
+                    cmap = cmap2,
+                    origin='lower')
+
+#v = np.linspace(0, N-1, 6,endpoint=True)
+#plt.xticks(v)
+#plt.yticks(v)
+plt.xlabel('$X_c$',{'fontsize':23})
+plt.ylabel('$Y_c$',{'fontsize':23})
+plt.tick_params(axis='both', which='major', labelsize=15)
+
+v = np.linspace(0, max(imagedata.flatten()), 6,endpoint=True)
+cbaxes = fig.add_axes([0.82, 0.1, 0.03, 0.8]) #[left,bottom,width,height] 
+cb=plt.colorbar(img2,cmap=cmap2,cax=cbaxes,ticks=v)
+cb.ax.tick_params(labelsize=15) 
+cb.set_label(label='$log_{10}(T)$',size=23,weight='bold')
+
+fig.savefig("image.png",
+            bbox_inches='tight', 
+            transparent=True,
+            pad_inches=0)
